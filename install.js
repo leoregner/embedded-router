ebccd2c5cdd28c43d685c30833da7318ffd80dbd
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2020-08-26 21:32:15 +0200

    local copy of jquery

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ const fs = require("fs"), https = require("https");
 
 // download jQuery
 let out = fs.createWriteStream("embedded-router.min.js");
-https.get("https://code.jquery.com/jquery-2.2.4.min.js", response => response.pipe(out));
+fs.createReadStream("jquery-2.2.4.min.js").pipe(out);
 
 // once done, append embedded-router.js
 out.on("close", () =>
